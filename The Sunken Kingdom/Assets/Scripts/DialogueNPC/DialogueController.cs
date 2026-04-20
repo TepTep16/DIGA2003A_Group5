25004e7fb259f3eac297eef3049c0fc147867c9e
Ilhaam-Jaffar <ilhaam2305@gmail.com>
2026-04-20 16:35:21 +0200

    updated dialogue controller code

--- The Sunken Kingdom/Assets/Scripts/DialogueNPC/DialogueController.cs
+++ The Sunken Kingdom/Assets/Scripts/DialogueNPC/DialogueController.cs
@@ -2,7 +2,6 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
-using static UnityEditor.Rendering.MaterialUpgrader;
 
 public class DialogueController : MonoBehaviour
 {
